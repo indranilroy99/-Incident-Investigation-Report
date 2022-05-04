2396cd7073c03c3ca6bda3e7c7a940cb3b972ea1
Indranil Roy <81170328+indranilroy99@users.noreply.github.com>
2022-05-04 21:35:49 +0530

    Add files via upload

---
+++ ireport.sh
@@ -0,0 +1,21 @@
+
+ #!/bin/bash
+echo Incident Investigation Report
+echo ====    Start Date and Time   ==== 
+date
+echo ====    System Information    ==== 
+uname -a
+echo ====    Network Interface     ====
+ifconfig -a
+echo ====    Network Statistics    ====
+netstat -ano | more
+echo ====     Processes            ====
+ps -axu
+echo ====    Routing Table         ====
+route -n
+echo ====  Last failed login Attempts ====
+last -f /var/log/btmp
+echo ====  currently logged in users  ====
+last -f /var/log/wtmp
+echo ====        End Date           ====
+date 
